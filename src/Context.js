72c884ed15cbd2c6a3186f861e1be89a19c444a7
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-06-28 07:18:28 +0200

    create Context.js file
    - create Context file and add fake data for testing

---
+++ src/Context.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+class Provider extends Component {
+  state = {
+    track_list: [
+      { track: { track_name: 'ABC' } },
+      { track: { track_name: 'ddfg' } },
+    ],
+    heading: 'Top 10 Tracks'
+  }
+  render() {
+    return (
+      <Context.Provider value={ this.state }>
+        { this.props.children }
+      </Context.Provider>
+    );
+  }
+};
+
+export Provider;
+export const Consumer = Context.Consumer
