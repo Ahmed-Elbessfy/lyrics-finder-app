6d1ad378e84dad7b9e2e48a1b9e1206a3e622773
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-07 14:28:05 +0200

    fix warn in Tracks comp
    - use '===' insted of '==' in render condition

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -7,25 +7,25 @@ class Tracks extends Component {
   render() {
     return (
       <Consumer>
-        {value => {
+        { value => {
           let { track_list, heading } = value;
-          if (track_list == undefined || track_list.length == 0) {
+          if (track_list === undefined || track_list.length === 0) {
             return <Spinner />;
           } else {
             return (
               <React.Fragment>
-                <h3 className="text-center mb-4">{heading}</h3>
+                <h3 className="text-center mb-4">{ heading }</h3>
                 <div className="row">
-                  {track_list.map(item => {
+                  { track_list.map(item => {
                     return (
-                      <Track key={item.track.track_id} track={item.track} />
+                      <Track key={ item.track.track_id } track={ item.track } />
                     );
-                  })}
+                  }) }
                 </div>
               </React.Fragment>
             );
           }
-        }}
+        } }
       </Consumer>
     );
   }
