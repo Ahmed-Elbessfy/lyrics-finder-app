6caa98e9867e66cb7fc7688854acc8e116bfbb5a
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-07 08:11:35 +0200

    add spinner and set Tracks content
    - add Spinner comp and Spinner gif
    - add Tracks comp. content to display Spinner till recieve data from Context

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import { Consumer } from '../../Context'
+import React, { Component } from "react";
+import { Consumer } from "../../Context";
+import Spinner from "../layout/Spinner";
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
-        { value => {
-          console.log(value)
-        } }
+        {value => {
+          let { track_list, heading } = value;
+          if (track_list == undefined || track_list.length == 0) {
+            return <Spinner />;
+          } else {
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => {
+                    console.log(item.track);
+                  })}
+                </div>
+              </React.Fragment>
+            );
+          }
+        }}
       </Consumer>
     );
   }
