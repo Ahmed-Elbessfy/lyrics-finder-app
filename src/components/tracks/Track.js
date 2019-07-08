8c892b85e623d878fbf7f30ccbfd75a6bd95ebbc
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-08 11:12:42 +0200

    fix lyrics request bug
    - pass a track id to lyrics request instead of 'id' word

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -4,28 +4,28 @@ import { Link } from "react-router-dom";
 function Track(props) {
   const { track } = props;
   return (
-    <div className="col-md-6" style={{ minHeight: "100px" }}>
+    <div className="col-md-6" style={ { minHeight: "100px" } }>
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
           <h4 className="card-title ">
             <i className="fa fa-play" aria-hidden="true" />
-            {" " + track.track_name}
+            { " " + track.track_name }
           </h4>
           <div className="card-text">
             <p>
               <strong>
                 <i className="fas fa-user" /> Artist:
               </strong>
-              {" " + track.artist_name}
+              { " " + track.artist_name }
             </p>
             <p>
               <strong>
-                <i className="fas fa-compact-disc" /> Album:{" "}
+                <i className="fas fa-compact-disc" /> Album:{ " " }
               </strong>
-              {track.album_name}
+              { track.album_name }
             </p>
           </div>
-          <Link className="btn btn-dark btn-block" to={"lyrics/track/:id"}>
+          <Link className="btn btn-dark btn-block" to={ `lyrics/track/${track.track_id}` }>
             <i className="fas fa-chevron-right" /> View Lyrics
           </Link>
         </div>
