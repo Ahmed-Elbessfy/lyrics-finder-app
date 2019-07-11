6ec33eee0ca8b4c4399a0cd9efc0970d418d9c95
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-11 23:08:18 +0200

    Updates

---
+++ precache-manifest.484cc454241f189c0821dc885323fe2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077310d215a6d74137018741ba4757d7",
+    "url": "/lyrics-finder-app/index.html"
+  },
+  {
+    "revision": "faf955d7f69b75496145",
+    "url": "/lyrics-finder-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "572c4249d2c60dd3818f",
+    "url": "/lyrics-finder-app/static/js/2.223abb59.chunk.js"
+  },
+  {
+    "revision": "faf955d7f69b75496145",
+    "url": "/lyrics-finder-app/static/js/main.12e9597c.chunk.js"
+  },
+  {
+    "revision": "b40128b5ff32693f41e3",
+    "url": "/lyrics-finder-app/static/js/runtime~main.61c68fea.js"
+  }
+]);
